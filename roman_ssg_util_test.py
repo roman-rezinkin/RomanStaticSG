568bcdcd97e91bff9f78274cfbe3ebc4e75047f0
roman-rezinkin <roman.rezinkin@hotmail.com>
2021-11-14 17:25:56 -0500

    Fixed tests

--- roman_ssg_util_test.py
+++ roman_ssg_util_test.py
@@ -34,7 +34,10 @@ def test_create_css_file():
 
 def test_write_to_file():
     """Test write_to_file function"""
-    filePath = os.getcwd() + "\\" + "example.txt"
+    if os.name == "posix":
+        filePath = os.getcwd() + "/" + "example.txt"
+    else:
+        filePath = os.getcwd() + "\\" + "example.txt"
     roman_ssg_util.write_to_file(
         "en-CA",
         filePath,
@@ -50,13 +53,22 @@ def test_conversion_func_file_non_custom_dir():
     fileArr = []
     fileArr.append("example.txt")
     fileArr.append("test.md")
-    roman_ssg_util.conversion_func_file(
-        "en-CA",
-        False,
-        fileArr,
-        "",
-        os.getcwd() + "\\" + "dist",
-    )
+    if os.name == "posix":
+        roman_ssg_util.conversion_func_file(
+            "en-CA",
+            False,
+            fileArr,
+            "",
+            os.getcwd() + "/" + "dist",
+        )
+    else:
+        roman_ssg_util.conversion_func_file(
+            "en-CA",
+            False,
+            fileArr,
+            "",
+            os.getcwd() + "\\" + "dist",
+        )
     assert os.path.isfile("example.html")
     assert os.path.isfile("test.html")
 
@@ -69,14 +81,22 @@ def test_conversion_func_file_custom_dir():
     fileArr.append("example.txt")
     fileArr.append("test.md")
     os.mkdir("testCustomDirectory")
-
-    roman_ssg_util.conversion_func_file(
-        "en-CA",
-        True,
-        fileArr,
-        os.getcwd() + "\\" + "testCustomDirectory",
-        os.getcwd() + "\\" + "dist",
-    )
+    if os.name == "posix":
+        roman_ssg_util.conversion_func_file(
+            "en-CA",
+            True,
+            fileArr,
+            os.getcwd() + "/" + "testCustomDirectory",
+            os.getcwd() + "/" + "dist",
+        )
+    else:
+        roman_ssg_util.conversion_func_file(
+            "en-CA",
+            True,
+            fileArr,
+            os.getcwd() + "\\" + "testCustomDirectory",
+            os.getcwd() + "\\" + "dist",
+        )
     assert os.path.isfile("example.html")
     assert os.path.isfile("test.html")
 
@@ -91,15 +111,24 @@ def test_converstion_func_folder_non_custom_dir():
     arrayOfFiles.append("The Adventure of the Speckled Band.txt")
     arrayOfFiles.append("The Naval Treaty.txt")
     arrayOfFiles.append("The Red Headed League.txt")
-
-    roman_ssg_util.conversion_func_folder(
-        "en-CA",
-        os.getcwd() + "\\" + "Sherlock-Holmes-Selected-Stories",
-        False,
-        arrayOfFiles,
-        "",
-        os.getcwd() + "\\" + "dist",
-    )
+    if os.name == "posix":
+        roman_ssg_util.conversion_func_folder(
+            "en-CA",
+            os.getcwd() + "/" + "Sherlock-Holmes-Selected-Stories",
+            False,
+            arrayOfFiles,
+            "",
+            os.getcwd() + "/" + "dist",
+        )
+    else:
+        roman_ssg_util.conversion_func_folder(
+            "en-CA",
+            os.getcwd() + "\\" + "Sherlock-Holmes-Selected-Stories",
+            False,
+            arrayOfFiles,
+            "",
+            os.getcwd() + "\\" + "dist",
+        )
     assert os.path.isfile("Silver Blaze.html")
     assert os.path.isfile("The Adventure of the Six Napoleans.html")
     assert os.path.isfile("The Adventure of the Speckled Band.html")
@@ -123,15 +152,24 @@ def test_converstion_func_folder_custom_dir():
         os.mkdir("testCustomDirectory")
     else:
         os.mkdir("testCustomDirectory")
-
-    roman_ssg_util.conversion_func_folder(
-        "en-CA",
-        os.getcwd() + "\\" + "Sherlock-Holmes-Selected-Stories",
-        True,
-        arrayOfFiles,
-        os.getcwd() + "\\" + "testCustomDirectory",
-        os.getcwd() + "\\" + "dist",
-    )
+    if os.name == "posix":
+        roman_ssg_util.conversion_func_folder(
+            "en-CA",
+            os.getcwd() + "/" + "Sherlock-Holmes-Selected-Stories",
+            True,
+            arrayOfFiles,
+            os.getcwd() + "/" + "testCustomDirectory",
+            os.getcwd() + "/" + "dist",
+        )
+    else:
+        roman_ssg_util.conversion_func_folder(
+            "en-CA",
+            os.getcwd() + "\\" + "Sherlock-Holmes-Selected-Stories",
+            True,
+            arrayOfFiles,
+            os.getcwd() + "\\" + "testCustomDirectory",
+            os.getcwd() + "\\" + "dist",
+        )
     assert os.path.isfile("Silver Blaze.html")
     assert os.path.isfile("The Adventure of the Six Napoleans.html")
     assert os.path.isfile("The Adventure of the Speckled Band.html")
